0d8f894bc165dc5dd282d7335d92fefbda73a30b
“a-vecchi” <alexandre_v@yahoo.com>
2021-06-20 13:57:06 -0300

    Criando Projeto

---
+++ src/main/java/com/example/demo/WhokshopMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhokshopMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhokshopMongodbApplication.class, args);
+	}
+
+}
